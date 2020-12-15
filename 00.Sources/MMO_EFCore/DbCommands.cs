2f1a2d7ad2285aa43287f129a5cbee3121edf940
SynK <syneffort@gmail.com>
2020-12-16 08:32:18 +0900

    CRUD 테스트

---
+++ 00.Sources/MMO_EFCore/DbCommands.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace MMO_EFCore
+{
+    public class DbCommands
+    {
+        // 초기화는 시간 소요 됨
+        public static void InitializeDB(bool forceReset = false)
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                if (!forceReset && (db.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
+                    return;
+
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                CreateTestData(db);
+                Console.WriteLine("DB was Initialized");
+            };
+        }
+
+        public static void CreateTestData(AppDbContext db)
+        {
+            Player player = new Player()
+            {
+                Name = "SynK"
+            };
+
+            List<Item> Items = new List<Item>()
+            {
+                new Item()
+                {
+                    TemplateId = 101,
+                    CreateDate = DateTime.Now,
+                    Owner = player
+                },
+                new Item()
+                {
+                    TemplateId = 102,
+                    CreateDate = DateTime.Now,
+                    Owner = player
+                },
+                new Item()
+                {
+                    TemplateId = 103,
+                    CreateDate = DateTime.Now,
+                    Owner = new Player() { Name = "Faker" }
+                },
+            };
+
+            db.Items.AddRange(Items);
+            db.SaveChanges();
+        }
+
+        public static void ReadAll()
+        {
+            using (AppDbContext db = new AppDbContext())
+            {
+                // AsNoTracking : ReadOnly => Tracking Snapshot 이라는 데이터 변경 탐지기능 때문
+                // Include : Eager Loading (즉시 로딩)
+                foreach (Item item in db.Items.AsNoTracking().Include(i => i.Owner))
+                {
+                    Console.WriteLine($"TempalteId({item.TemplateId}) Owner({item.Owner.Name}) Created({item.CreateDate})");
+                }
+            };
+        }
+
+        // 특정 플레이어가 소지한 아이템들의 CreatedDate 수정
+        public static void UpdateDate()
+        {
+            Console.WriteLine("Input Player Name");
+            Console.Write("> ");
+            string name = Console.ReadLine();
+
+            using (AppDbContext db = new AppDbContext())
+            {
+                var items = db.Items.Include(i => i.Owner)
+                            .Where(i => i.Owner.Name == name);
+
+                foreach (Item item in items)
+                {
+                    item.CreateDate = DateTime.Now;
+                }
+
+                db.SaveChanges();
+            }
+
+            ReadAll();
+        }
+
+        public static void DeleteItem()
+        {
+            Console.WriteLine("Input Player Name");
+            Console.Write("> ");
+            string name = Console.ReadLine();
+
+            using (AppDbContext db = new AppDbContext())
+            {
+                var items = db.Items.Include(i => i.Owner)
+                            .Where(i => i.Owner.Name == name);
+
+                db.Items.RemoveRange(items);
+
+                db.SaveChanges();
+            }
+
+            ReadAll();
+        }
+    }
+}
--- 00.Sources/MMO_EFCore/Program.cs
+++ 00.Sources/MMO_EFCore/Program.cs
@@ -1,29 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
-using System;
+﻿using System;
 
 namespace MMO_EFCore
 {
     class Program
     {
-        // 초기화는 시간 소요 됨
-        static void InitializeDB(bool forceReset = false)
-        {
-            using (AppDbContext db = new AppDbContext())
-            {
-                if (!forceReset && (db.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists())
-                    return;
-
-                db.Database.EnsureDeleted();
-                db.Database.EnsureCreated();
-
-                Console.WriteLine("DB was Initialized");
-            };
-        }
-
         static void Main(string[] args)
         {
-            InitializeDB(forceReset: true);
+            DbCommands.InitializeDB(forceReset: false);
+
+            // CRUD
+            Console.WriteLine("명령어를 입력하세요");
+            Console.WriteLine("[0] Force reset");
+            Console.WriteLine("[1] Read all");
+            Console.WriteLine("[2] Update date");
+            Console.WriteLine("[3] Delete item");
+
+            while (true)
+            {
+                Console.Write("> ");
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "0":
+                        DbCommands.InitializeDB(forceReset: true);
+                        break;
+                    case "1":
+                        DbCommands.ReadAll();
+                        break;
+                    case "2":
+                        DbCommands.UpdateDate();
+                        break;
+                    case "3":
+                        DbCommands.DeleteItem();
+                        break;
+                }
+            }
         }
     }
 }
